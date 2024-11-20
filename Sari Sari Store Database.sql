77bf64b2283f5d75247340ca78199df1c5af0945
iverene <causapiniverenegrace@gmail.com>
2024-11-20 16:06:39 +0800

    Add files via upload

---
+++ Sari Sari Store Database.sql
@@ -0,0 +1,40 @@
+/*create database DbProducts;*/
+
+/*create table TbProducts (
+ProductNumber int primary key auto_increment,
+ProductName varchar(20) not null,
+ProductType varchar(20) not null,
+Price int not null,
+Stocks int not null
+);*/
+
+/*INSERT INTO TbProducts (ProductName, ProductType, Price, Stocks)
+VALUES
+    ('Safeguard', 'Soap', 30, 30),
+    ('Silka', 'Soap', 25, 25),
+    ('Sunsilk', 'Shampoo', 10, 36),
+    ('Palmolive', 'Shampoo', 10, 40),
+    ('Head & Shoulders', 'Shampoo', 10, 30),
+    ('Surf', 'Detergent Powder', 12, 25),
+    ('Tide', 'Detergent Powder', 12, 29),
+    ('Pride', 'Bar Soap', 23, 20),
+    ('Perla', 'Bar Soap', 25, 21),
+    ('Downy', 'Fabric Conditioner', 12, 31),
+    ('Rebisco', 'Biscuit', 10, 25),
+    ('Bread Stix', 'Biscuit', 10, 14),
+    ('Sky Flakes', 'Biscuit', 10, 20),
+    ('Piattos', 'Snack', 20, 10),
+    ('Oishi', 'Snack', 10, 12),
+    ('Onion Rings', 'Snack', 10, 10),
+    ('Coke Mismo', 'Cold Beverage', 20, 20),
+    ('Coke 1.5L', 'Cold Beverage', 80, 10),
+    ('Sting', 'Cold Beverage', 20, 10),
+    ('Mountain Dew', 'Cold Beverage', 20, 10),
+    ('Tang', 'Powdered Beverage', 20, 12),
+    ('Milo', 'Powdered Beverage', 10, 26),
+    ('Bear Brand', 'Powdered Beverage', 10, 25),
+    ('Great Taste White', 'Powdered Beverage', 10, 15),
+    ('Kopiko', 'Powdered Beverage', 15, 19);*/
+    
+select * from TbProducts;
+
